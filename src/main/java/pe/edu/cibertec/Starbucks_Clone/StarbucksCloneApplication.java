bb527db1c6b4d35f89b6ff02f93ed6eff24e7c1c
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-07-26 11:13:58 -0500

    frist commit

---
+++ src/main/java/pe/edu/cibertec/Starbucks_Clone/StarbucksCloneApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Starbucks_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Starbucks_Clone/StarbucksCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Starbucks_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
